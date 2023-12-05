06593d5d63edd9e37d86b787f39ecc920cffa4e5
Håkon Østerbø <hauk88@gmail.com>
2023-12-05 19:48:20 +0100

    split into part 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,31 +1,4 @@
+mod part1;
 fn main() {
-    println!("Hello, world!");
-    // read short.txt
-    let contents =
-        std::fs::read_to_string("long.txt").expect("Something went wrong reading the file");
-
-    // loop over contents
-    let mut sum = 0;
-    for line in contents.lines() {
-        // loop over each character
-        let chars: Vec<_> = line.chars().collect();
-        let mut num = String::from("");
-
-        for i in 0..chars.len() {
-            if chars[i].is_numeric() {
-                num.push(chars[i]);
-                break;
-            }
-        }
-
-        for i in (0..chars.len()).rev() {
-            if chars[i].is_numeric() {
-                num.push(chars[i]);
-                break;
-            }
-        }
-
-        sum += num.parse::<i32>().unwrap();
-    }
-    println!("sum: {}", sum)
+    part1::part1();
 }
---
+++ day1/src/part1.rs
@@ -0,0 +1,29 @@
+pub fn part1() {
+    let contents =
+        std::fs::read_to_string("longp1.txt").expect("Something went wrong reading the file");
+
+    // loop over contents
+    let mut sum = 0;
+    for line in contents.lines() {
+        // loop over each character
+        let chars: Vec<_> = line.chars().collect();
+        let mut num = String::from("");
+
+        for i in 0..chars.len() {
+            if chars[i].is_numeric() {
+                num.push(chars[i]);
+                break;
+            }
+        }
+
+        for i in (0..chars.len()).rev() {
+            if chars[i].is_numeric() {
+                num.push(chars[i]);
+                break;
+            }
+        }
+
+        sum += num.parse::<i32>().unwrap();
+    }
+    println!("sum: {}", sum)
+}

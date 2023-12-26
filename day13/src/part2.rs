768dd7720c20521e99231c1674ec5bebf4e82a00
Håkon Østerbø <hauk88@gmail.com>
2023-12-26 15:56:46 +0100

    d13 p2

---
+++ day13/src/part2.rs
@@ -0,0 +1,79 @@
+use std::cmp::min;
+
+pub fn part2() {
+    let content = std::fs::read_to_string("longp1.txt").unwrap();
+    let lines = content.lines();
+    let mut puzzle: Vec<Vec<char>> = Vec::new();
+    let mut sum = 0;
+    for line in lines {
+        if line.trim().len() == 0 {
+            sum += find_reflection(&puzzle);
+            puzzle = Vec::new();
+            continue;
+        }
+        puzzle.push(line.chars().collect());
+    }
+    sum += find_reflection(&puzzle);
+
+    println!("{}", sum);
+}
+
+fn find_reflection(puzzle: &Vec<Vec<char>>) -> i32 {
+    let n = puzzle.len();
+    let m = puzzle[0].len();
+
+    for idx in 0..m - 1 {
+        let mut miss = 0;
+
+        let width = min(idx + 1, m - idx - 1);
+
+        for w_idx in 0..width {
+            let left = idx - w_idx;
+            let right = idx + 1 + w_idx;
+
+            for i in 0..n {
+                if puzzle[i][left] != puzzle[i][right] {
+                    miss += 1;
+                    if miss > 1 {
+                        break;
+                    }
+                }
+            }
+            if miss > 1 {
+                break;
+            }
+        }
+
+        if miss == 1 {
+            return idx as i32 + 1;
+        }
+    }
+
+    for idx in 0..(n - 1) {
+        let mut miss = 0;
+        let width = min(idx + 1, n - idx - 1);
+
+        for w_idx in 0..width {
+            let left = idx - w_idx;
+            let right = idx + 1 + w_idx;
+
+            for j in 0..m {
+                if puzzle[left][j] != puzzle[right][j] {
+                    miss += 1;
+                    if miss > 1 {
+                        break;
+                    }
+                }
+            }
+            if miss > 1 {
+                break;
+            }
+        }
+
+        if miss == 1 {
+            return 100 * (idx as i32 + 1);
+        }
+    }
+
+    panic!("No reflection found")
+}

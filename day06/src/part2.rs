c94a617c094b30aa988c9383bf543ca29daf39c3
Håkon Østerbø <hauk88@gmail.com>
2024-12-07 13:43:57 +0100

    clean up solution

--- day06/src/part2.rs
+++ day06/src/part2.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, fs, time::Instant};
+use std::{collections::HashSet, fs};
 
 pub fn part2() {
     let cont = fs::read_to_string("largep1.txt").unwrap();
@@ -20,7 +20,6 @@ pub fn part2() {
     });
 
     let mut res = 0;
-    let now = Instant::now();
     for i in 0..(n + 1) {
         for j in 0..(m + 1) {
             let new_obstical = (i as i32, j as i32);
@@ -31,56 +30,44 @@ pub fn part2() {
                 continue;
             }
             map.insert(new_obstical);
-
-            let mut visited: HashSet<(i32, i32, i32, i32)> = HashSet::new();
-            let mut current = start.clone();
-            let mut direction = (-1, 0);
-
-            let mut is_loop = false;
-            loop {
-                let poshash = (current.0, current.1, direction.0, direction.1);
-                if visited.contains(&poshash) {
-                    is_loop = true;
-                    break;
-                }
-                visited.insert(poshash);
-
-                let next = (current.0 + direction.0, current.1 + direction.1);
-                if next.0 < 0 || next.1 < 0 || next.0 > n || next.1 > m {
-                    break;
-                }
-                if map.contains(&next) {
-                    direction = match direction {
-                        (-1, 0) => (0, 1),
-                        (0, 1) => (1, 0),
-                        (1, 0) => (0, -1),
-                        (0, -1) => (-1, 0),
-                        _ => panic!("no dir"),
-                    }
-                } else {
-                    current = next;
-                }
-            }
-            if is_loop {
-                res = res + 1;
+            if check_loop(&map, start, n, m) {
+                res += 1;
             }
 
             map.remove(&new_obstical);
         }
     }
-    let elapsed = Instant::now() - now;
-    println!("{:?}", elapsed);
     println!("{}", res);
 }
 
-fn print_map(map: HashSet<(i32, i32)>, n: i32, m: i32) {
-    for i in 0..n {
-        for j in 0..m {
-            match map.contains(&(i as i32, j as i32)) {
-                true => print!("#"),
-                false => print!("."),
-            }
+fn check_loop(map: &HashSet<(i32, i32)>, start: (i32, i32), n: i32, m: i32) -> bool {
+    let mut visited: HashSet<(i32, i32, i32, i32)> = HashSet::new();
+    let mut current = start.clone();
+    let mut direction = (-1, 0);
+
+    loop {
+        let poshash = (current.0, current.1, direction.0, direction.1);
+        if visited.contains(&poshash) {
+            return true;
         }
-        println!("")
+        visited.insert(poshash);
+
+        let next = (current.0 + direction.0, current.1 + direction.1);
+        if next.0 < 0 || next.1 < 0 || next.0 > n || next.1 > m {
+            break;
+        }
+        if map.contains(&next) {
+            direction = match direction {
+                (-1, 0) => (0, 1),
+                (0, 1) => (1, 0),
+                (1, 0) => (0, -1),
+                (0, -1) => (-1, 0),
+                _ => panic!("no dir"),
+            }
+        } else {
+            current = next;
+        }
     }
+
+    return false;
 }

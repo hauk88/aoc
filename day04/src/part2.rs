1459f9ef3e8a701731a2edce97cc0e6e37b72660
Håkon Østerbø <hauk88@gmail.com>
2025-12-04 19:29:57 +0100

    use HashSet instead of HashMap

--- day04/src/part2.rs
+++ day04/src/part2.rs
@@ -1,12 +1,12 @@
-use std::{collections::hash_map::HashMap, fs};
+use std::{collections::HashSet, fs};
 
 pub fn part2() {
     let cont = fs::read_to_string("largep1.txt").unwrap();
-    let mut map: HashMap<(i32, i32), char> = HashMap::new();
+    let mut map: HashSet<(i32, i32)> = HashSet::new();
     cont.lines().enumerate().for_each(|(i, line)| {
         line.chars().enumerate().for_each(|(j, c)| {
             if c == '@' {
-                map.insert((i as i32, j as i32), c);
+                map.insert((i as i32, j as i32));
             }
         });
     });
@@ -14,12 +14,12 @@ pub fn part2() {
     let total_paper = map.len();
     loop {
         let n = map.len();
-        let remaining_keys: Vec<(i32, i32)> = map.keys().cloned().collect();
+        let remaining_keys: Vec<(i32, i32)> = map.iter().cloned().collect();
         remaining_keys.iter().for_each(|(i, j)| {
             let neighbours = get_neighbors(i, j);
             let mut n_count = 0;
             for n in neighbours {
-                if map.contains_key(&n) {
+                if map.contains(&n) {
                     n_count += 1;
                     if n_count > 3 {
                         break;

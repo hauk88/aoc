395ec91a2393f731db9c02ef7801440231a5816e
Håkon Østerbø <hauk88@gmail.com>
2025-12-09 22:21:20 +0100

    d9 p1 done. only scaffolding for p2

---
+++ day09/src/part2.rs
@@ -0,0 +1,42 @@
+use std::fs;
+
+pub fn part2() {
+    let cont = fs::read_to_string("largep1.txt").unwrap();
+
+    let cords: Vec<Vec2d> = cont
+        .lines()
+        .map(|line| {
+            let parts: Vec<i64> = line.split(',').map(|s| s.parse::<i64>().unwrap()).collect();
+            Vec2d {
+                x: parts[0],
+                y: parts[1],
+            }
+        })
+        .collect();
+
+    let mut areas: Vec<i64> = Vec::new();
+    let n = cords.len();
+    for i in 0..n {
+        for j in (i + 1)..n {
+            let area = cords[i].area(&cords[j]);
+            areas.push(area);
+        }
+    }
+    areas.sort();
+    println!("{}", areas.last().unwrap());
+}
+
+#[derive(Debug)]
+struct Vec2d {
+    x: i64,
+    y: i64,
+}
+
+impl Vec2d {
+    fn area(&self, other: &Vec2d) -> i64 {
+        let dx = (self.x - other.x).abs() + 1;
+        let dy = (self.y - other.y).abs() + 1;
+        let res = dx * dy;
+        return res;
+    }
+}
